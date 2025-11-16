361b1bc14dd5d8b56bbc578c1d4899a3261a15c8
Abdur <abdur1012141618@gmail.com>
2025-11-16 17:37:36 +0100

    Final Fix: Revert i18n import and add optimizeDeps to vite.config.ts

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector/cjs/i18nextBrowserLanguageDetector.js';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import en from '@/locales/en.json';
 import bn from '@/locales/bn.json';
